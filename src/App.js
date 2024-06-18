a87384b1846848e548d44b17f73bee6f272597a7
alwolmer <arthur.wolmer@gmail.com>
2024-06-18 11:28:41 -0300

    added expense list and allocation form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 
 // Add code to import the other components here under
 
@@ -27,28 +30,19 @@ const App = () => {
                         <div className='col-sm'>
                             <ExpenseTotal />
                         </div>
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
-                </div>
+                    </div>
+                    <h3 className='mt-3'>Allocation</h3>
+                    <div className='row'>
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>
+                    </div>
+                    <h3 className='mt-3'>Change allocation</h3>
+                    <div className='row mt-3'>
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>
+                    </div>
             </div>
         </AppProvider>
     );
