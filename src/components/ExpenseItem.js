3d05b28acbe3de0e169c5512eaa87a23f9e504a8
alwolmer <arthur.wolmer@gmail.com>
2024-06-18 22:29:29 -0300

    increase and decrease buttons working

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FaCircleMinus, FaCirclePlus } from "react-icons/fa6"
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -25,11 +26,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FaCirclePlus style={{color: 'green'}} role='button' onClick={() => increaseAllocation(props.name)} /></td>
+        <td><FaCircleMinus style={{color: 'red'}} role='button' onClick={() => decreaseAllocation(props.name)} /></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
